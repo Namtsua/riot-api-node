b9fa3de3df8e47d48b6815861f6a20859d1405c5
Mikhail Osher <yo@miraage.io>
2017-08-29 01:54:29 +0300

    Pass region to tests from environment variable

--- examples/client.js
+++ examples/client.js
@@ -1,5 +1,5 @@
 const Client = require('../lib/Client');
 
-const client = new Client(process.env.API_KEY, 'euw');
+const client = new Client(process.env.API_KEY, process.env.API_REGION);
 
 module.exports = client;
