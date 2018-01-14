5a214fa15b480eed45393ed5a4f18b3008ab596f
Spencer Austman <Namtsua@users.noreply.github.com>
2018-01-13 17:25:00 -0800

    Removed commas to allow program to run in default state.

--- examples/summoner.js
+++ examples/summoner.js
@@ -3,16 +3,16 @@ const log = require('./log');
 
 log(
   'SummonerByName(Algalon Observer)',
-  client.summoner.byName('Algalon Observer'),
+  client.summoner.byName('Algalon Observer')
 );
 
 log(
   'SummonerById(95299786)',
-  client.summoner.bySummonerId(95299786),
+  client.summoner.bySummonerId(95299786)
 );
 
 log(
   'SummonerByAccountId(231429628)',
-  client.summoner.byAccountId(231429628),
+  client.summoner.byAccountId(231429628)
 );
 
